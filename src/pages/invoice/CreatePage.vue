b023ae2bca6a5ab9c7ac63a10d6ec903b948c588
Majid Mohamadi <m.shutdown57@gmail.com>
2023-07-21 20:07:17 +0330

    Fixed invoice create page

--- src/pages/invoice/CreatePage.vue
+++ src/pages/invoice/CreatePage.vue
@@ -1,15 +1,38 @@
 <template lang="html">
   <q-page>
     <!-- TODO: Add total amount -->
-    <InvoiceFormComponent :invoice="invoice" :total-amount="0" @InvoiceValues="handleCreateInvoice" />
 
-    <q-card v-if="invoice && invoice.id" class="justify-center">
-      <q-card-section>
+    <q-stepper v-model="step" vertical animated>
+      <q-step :name="1" title="ایجاد فاکتور" icon="file-plus" :done="step > 1">
+        <InvoiceBaseFormComponent
+          :invoice="invoice"
+          @InvoiceValues="handleCreateInvoice"
+        />
+      </q-step>
+
+      <q-step :name="2" title="افزودن مشتری / فروشنده" icon="settings" :done="step > 2">
+        <!-- NEW Form to store customer or vendor -->
+        <InvoiceAccountCustomerVendorFormComponent
+          :invoice="invoice"
+          @SelectedValues="handleValues"
+        />
+      </q-step>
+
+      <q-step :name="3" title="افزودن محصولات" icon="settings" :done="step > 3">
+        <!-- Product form -->
         <div class="q-gutter-md column">
-          <ProductsComponent :invoice="invoice" />
+          <ProductsComponent :invoice="invoice" @total-amount="handleTotalAmount" />
         </div>
-      </q-card-section>
-    </q-card>
+      </q-step>
+    </q-stepper>
+
+    <!-- <q-card v-if="invoice && invoice.id" class="justify-center"> -->
+    <!--   <q-card-section> -->
+    <!--     <div class="q-gutter-md column"> -->
+    <!--       <ProductsComponent :invoice="invoice" /> -->
+    <!--     </div> -->
+    <!--   </q-card-section> -->
+    <!-- </q-card> -->
 
     <q-dialog v-model="alert" persistent transition-show="scale" transition-hide="scale">
       <q-card class="bg-red text-white" style="width: 300px">
@@ -32,18 +55,20 @@
 <script setup lang="ts">
 import { Invoice } from 'src/interface/invoice'
 import ProductsComponent from 'src/components/ProductsComponent.vue'
-import InvoiceFormComponent from 'src/components/forms/InvoiceFormComponent.vue'
+import InvoiceBaseFormComponent from 'src/components/forms/InvoiceBaseFormComponent.vue'
+import InvoiceAccountCustomerVendorFormComponent from 'src/components/forms/InvoiceAccountCustomerVendorFormComponent.vue'
 import { ref, watch, onMounted, computed } from 'vue'
 import { useStore } from 'src/store'
 import { useRoute, useRouter } from 'vue-router'
 
 const alert = ref(false)
+const step = ref(1)
 
 const store = useStore()
 const router = useRouter()
 const route = useRoute()
 
-const invoice = computed((): Invoice => store.getters['invoice/invoice'])
+const invoice = computed(() => store.getters['invoice/invoice'])
 
 watch(route.query, (value) => {
   if (value && value.id) {
@@ -64,8 +89,10 @@ const fetchData = async () => {
   await store.dispatch('account/getAccounts')
 }
 
-const handleCreateInvoice = async (data: Invoice) => {
-  if (!data.number) {
+const handleCreateInvoice = async ($event?: Invoice) => {
+  if (!$event) return false
+
+  if (!$event.number) {
     alert.value = true
     return false
   }
@@ -73,11 +100,42 @@ const handleCreateInvoice = async (data: Invoice) => {
   if (invoice.value && invoice.value.id) {
     await store.dispatch('invoice/updateInvoice', {
       id: invoice.value.id,
-      ...data
+      ...$event
     })
+    await store.dispatch('installment/setInstallments')
   } else {
-    await store.dispatch('invoice/createInvoice', data)
+    await store.dispatch('invoice/createInvoice', $event)
   }
   router.push({ path: '/invoice/create', query: { id: invoice.value.id } })
+  step.value = 2
+}
+
+const handleValues = async (
+  $event?: {
+    customer: { id?: number, name?: string },
+    vendor: { id?: number, name?: string },
+    account: { id?: number, name?: string }
+  }
+) => {
+  const payload = {
+    id: invoice.value.id,
+    accountId: $event?.account?.id ? $event.account.id : undefined,
+    customerId: $event?.customer?.id ? $event.customer.id : undefined,
+    vendorId: $event?.vendor?.id ? $event.vendor.id : undefined
+  }
+  await store.dispatch('invoice/updateInvoice', payload)
+  await store.dispatch('installment/setInstallments', {
+    id: invoice.value.id
+  })
+  step.value = 3
+}
+
+const handleTotalAmount = async ($event?: number) => {
+  if (!$event) return
+  await store.dispatch('invoice/updateInvoice', {
+    id: invoice.value.id,
+    totalAmount: $event
+  })
+  router.push({ path: `/invoice/show/${invoice.value.id}` })
 }
 </script>
