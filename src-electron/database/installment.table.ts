5f92edb4009b9891794af40c5a8044442f90ac3d
Majid Mohamadi <m.shutdown57@gmail.com>
2023-07-21 21:19:33 +0330

    Cleanup

--- src-electron/database/installment.table.ts
+++ src-electron/database/installment.table.ts
@@ -1,15 +1,16 @@
 import { PrismaClient } from '@prisma/client'
 
-import { InstallmentCreate, InstallmentUpdate } from '../../src/interface/installment'
+import { InstallmentUpdate } from '../../src/interface/installment'
+import { InvoiceInstallment } from 'src/interface/invoice'
 
 export class Installment {
   private prisma
 
-  constructor () {
+  constructor() {
     this.prisma = new PrismaClient()
   }
 
-  async all (limit: number = 20, offset: number = 0): Promise<any> {
+  async all(limit: number = 20, offset: number = 0): Promise<any> {
     return await this
       .prisma
       .installment
@@ -24,25 +25,37 @@ export class Installment {
       })
   }
 
-  async create (payload: InstallmentCreate): Promise<void> {
-    await this.prisma.installment.create({ data: payload })
+  async create(payload: InvoiceInstallment): Promise<void> {
+    console.log(payload)
+    // await this.prisma.installment.create({ data: payload })
+    // await this.prisma.installment.upsert({
+    //   where: {  }
+    // })
   }
 
-  async byInvoiceId (id: number): Promise<any> {
+  // async createAll (payload: Array<InstallmentCreate>) {
+  //   await this.prisma.installment.createMany({
+  //     data: payload,
+  //     skipDuplicates: true
+  //   })
+  // }
+
+
+  async byInvoiceId(id: number): Promise<any> {
     return await this.prisma.installment.findFirst({
       where: { id },
       include: { customer: true }
     })
   }
 
-  async byId (id: number): Promise<any> {
+  async byId(id: number): Promise<any> {
     return await this.prisma.installment.findFirst({
       where: { id },
       include: { invoice: true, customer: true }
     })
   }
 
-  async update (payload: InstallmentUpdate): Promise<void> {
+  async update(payload: InstallmentUpdate): Promise<void> {
     const { id, ...data } = payload
     await this.prisma.installment.update({
       where: { id },
