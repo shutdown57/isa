5e52078e1a71c5f70ed115fec3005c98868b2c1c
Majid Mohamadi <m.shutdown57@gmail.com>
2023-07-21 20:04:58 +0330

    Fixed persian tools module issue

--- src/boot/persianTools.ts
+++ src/boot/persianTools.ts
@@ -1,21 +1,20 @@
 import { boot } from 'quasar/wrappers'
-import * as persianTools from '@persian-tools/persian-tools'
+import { digitsEnToFa, addCommas } from '@persian-tools/persian-tools'
 
-declare module '@vue/runtime-core' {
-  interface ComponentCustomProperties {
-    $persianTools: object;
-  }
-}
-
-declare module '@persian-tools/persian-tools' {
-  interface persianTools {
-    persianTools: object;
-  }
-}
+// declare module '@vue/runtime-core' {
+//   interface ComponentCustomProperties {
+//     $persianTools: object;
+//   }
+// }
 
 // "async" is optional;
 // more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async ({ app }) => {
-  app.config.globalProperties.$persianTools = persianTools
-  app.provide('persianTools', persianTools)
+export default boot(async () => {
+//   app.config.globalProperties.$persianTools = persianTools
+//   app.provide('persiantools.enToFa', digitsEnToFa as DigitsConverter)
 })
+
+export {
+  digitsEnToFa,
+  addCommas
+}
