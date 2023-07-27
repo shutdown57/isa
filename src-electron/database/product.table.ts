e0b06f4e096134b371d03ee05a171a7fcbc4f49a
Majid Mohamadi <m.shutdown57@gmail.com>
2023-07-27 19:40:24 +0330

    Added update product method

--- src-electron/database/product.table.ts
+++ src-electron/database/product.table.ts
@@ -1,23 +1,32 @@
 import { PrismaClient } from '@prisma/client'
 
-import { ProductCreate } from '../../src/interface/product'
+import { ProductCreate, ProductUpdate } from 'src/interface/product'
 
 export class Product {
   private prisma
 
-  constructor () {
+  constructor() {
     this.prisma = new PrismaClient()
   }
 
-  async all (limit: number = 20, offset: number = 0): Promise<any> {
+  async all(limit: number = 20, offset: number = 0): Promise<any> {
     return await this.prisma.product.findMany({ skip: offset, take: limit, orderBy: { name: 'asc' } })
   }
 
-  async create (payload: ProductCreate) {
+  async create(payload: ProductCreate) {
     await this.prisma.product.create({ data: payload })
   }
 
-  async byId (id: number) {
+  async update(payload: ProductUpdate) {
+    console.log(payload)
+    const { id, ...data } = payload
+    await this.prisma.product.update({
+      where: { id },
+      data
+    })
+  }
+
+  async byId(id: number) {
     return await this.prisma.product.findUnique({
       where: { id },
       include: {
@@ -26,7 +35,7 @@ export class Product {
     })
   }
 
-  async search (needle: string) {
+  async search(needle: string) {
     return await this.prisma.product.findMany({
       where: {
         name: { contains: needle }
