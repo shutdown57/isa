6d73aacc55d0a61e5ad4a8378e55c0ad81b3ac02
Majid Mohamadi <m.shutdown57@gmail.com>
2023-08-02 17:43:16 +0330

    Added sample expense index page

---
+++ src/pages/expense/IndexPage.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <q-page>
+    <q-card>
+      <q-card-section>
+        <q-btn
+          color="primary"
+          label="افزودن فاکتور"
+        />
+          <!-- @click.prevent="() => router.push({ path: '/invoice/create' })" -->
+      </q-card-section>
+    </q-card>
+
+    <q-card>
+      <q-card-section>
+        Expenses: {{ expenses.length }}
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+import { computed, onMounted } from 'vue'
+import { useStore } from 'src/store'
+
+const store = useStore()
+
+const expenses = computed(() => store.getters['expense/expenses'])
+</script>
