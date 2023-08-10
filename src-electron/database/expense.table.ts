ca73bd243560b35e6c8b18adee4ca24fc0e4a2ee
Majid Mohamadi <m.shutdown57@gmail.com>
2023-08-10 18:20:06 +0330

    Fixed issues
    Added remove method
    Cleanup

--- src-electron/database/expense.table.ts
+++ src-electron/database/expense.table.ts
@@ -11,10 +11,11 @@ export class Expense {
 
   async all (limit: number = 20, offset: number = 0): Promise<any> {
     return await this.prisma.expense.findMany({
-        skip: offset,
-        take: limit,
-        orderBy: { createdAt: 'desc' }
-      })
+      skip: offset,
+      take: limit,
+      orderBy: { createdAt: 'desc' },
+      include: { account: true }
+    })
   }
 
   async create (payload: ExpenseCreate) {
@@ -23,16 +24,17 @@ export class Expense {
   }
 
   async byId (id: number) {
-    return await this.prisma.expense.findUnique({
-      where: { id: id }
+    return await this.prisma.expense.findFirst({
+      where: { id: parseInt(`${id}`) },
+      include: { account: true }
     })
   }
 
   async update (payload: ExpenseUpdate) {
-    const { id, ...data } = payload
+    const { id, amount, ...data } = payload
     await this.prisma.expense.update({
       where: { id: parseInt(`${id}`) },
-      data
+      data: { ...data, amount: parseInt(`${amount}`) }
     })
   }
 
@@ -46,7 +48,11 @@ export class Expense {
     })
   }
 
-  async count() {
+  async count () {
     return await this.prisma.expense.count()
+  }
+
+  async remove (id: number): Promise<void> {
+    await this.prisma.expense.delete({ where: { id: parseInt(`${id}`) } })
   }
 }
