ae1d25d8aaef6c4bba7e2767f5b0d8e57e454984
Majid Mohamadi <m.shutdown57@gmail.com>
2023-08-10 18:18:54 +0330

    Added account name and delete button

--- src/pages/expense/IndexPage.vue
+++ src/pages/expense/IndexPage.vue
@@ -16,6 +16,7 @@
           <thead>
             <tr>
               <th class="text-center">کد</th>
+              <th class="text-center">حساب</th>
               <th class="text-center">عنوان</th>
               <th class="text-center">مبلغ</th>
               <th class="text-center">تاریخ ایجاد</th>
@@ -27,6 +28,7 @@
           <tbody>
             <tr v-for="expense in expenses" :key="expense.id">
               <td class="text-center">{{ digitsEnToFa(expense.id) }}</td>
+              <td class="text-center">{{ expense?.account?.name }}</td>
               <td class="text-center">{{ expense.title }}</td>
               <td class="text-center">{{ digitsEnToFa(addCommas(`${expense.amount}`)) }}</td>
               <td class="text-center">{{ digitsEnToFa(datetime(expense.createdAt)) }}</td>
@@ -36,6 +38,12 @@
                   color="info"
                   label="مشاهده"
                   @click.prevent="() => router.push({ path: '/expense/show/' + expense.id })"
+                />
+
+                <q-btn
+                  color="negative"
+                  label="حذف"
+                  @click.prevent="handleRemove(expense)"
                 />
               </td>
             </tr>
@@ -65,6 +73,7 @@ import { digitsEnToFa, addCommas } from 'src/boot/persianTools'
 import { datetime } from 'src/boot/jalaali'
 import { useStore } from 'src/store'
 import { useRouter, useRoute } from 'vue-router'
+import { Expense } from 'src/interface/expense'
 
 const store = useStore()
 const router = useRouter()
@@ -90,16 +99,8 @@ const totalPages = computed(() => {
   return result > 0 ? (total + 1) : total
 })
 
-onMounted(() => {
-  const { limit: l, offset: o, page: p } = route.query
-  if (l) limit.value = parseInt(`${l}`)
-  if (o) offset.value = parseInt(`${o}`)
-  if (p) current.value = parseInt(`${p}`)
-  store.dispatch('expense/getExpenses', {
-    limit: limit.value,
-    offset: offset.value
-  })
-  store.dispatch('expense/count')
+onMounted(async () => {
+  await fetchData()
 })
 
 watch(current, (value) => {
@@ -117,4 +118,26 @@ watch(current, (value) => {
     }
   })
 })
+
+const fetchData = async () => {
+  const { limit: l, offset: o, page: p } = route.query
+  if (l) limit.value = parseInt(`${l}`)
+  if (o) offset.value = parseInt(`${o}`)
+  if (p) current.value = parseInt(`${p}`)
+  await store.dispatch('expense/getExpenses', {
+    limit: limit.value,
+    offset: offset.value
+  })
+  await store.dispatch('expense/count')
+}
+
+const handleRemove = async (expense: Expense) => {
+  const { id, amount, accountId } = expense
+  await store.dispatch('account/amount', {
+    id: accountId,
+    amount: amount
+  })
+  await store.dispatch('expense/remove', id)
+  await fetchData()
+}
 </script>
