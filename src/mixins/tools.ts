a4ed092af16687264fecce06299bf1f3b9b986ef
Majid Mohamadi <m.shutdown57@gmail.com>
2023-08-17 16:37:16 +0330

    Added right types

--- src/mixins/tools.ts
+++ src/mixins/tools.ts
@@ -2,17 +2,17 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   methods: {
-    enToFa (value: string): string {
+    enToFa (value: string|number): string {
       // @ts-ignore
       return this.$persianTools.digitsEnToFa(value)
     },
 
-    addComma (value: string): string {
+    addComma (value: string|number): string {
       // @ts-ignore
       return this.$persianTools.addCommas(value)
     },
 
-    toStringValue (value: any): string {
+    toStringValue (value: number|string): string {
       return `${value}`
     }
   }
