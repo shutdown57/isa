80067b4e441c90de66ff094b78bb13781b1098ad
Majid Mohamadi <m.shutdown57@gmail.com>
2023-08-31 16:22:10 +0330

    Fixed price issues

--- src/interface/price.ts
+++ src/interface/price.ts
@@ -2,24 +2,24 @@ import { Product } from './product'
 
 export type Price = {
   id: number
-  productId: number
-  product: Product
-  buy: number
-  sell: number
-  percent: number
+  productId: number | null
+  product?: Product | null
+  buy: bigint | null
+  sell: bigint | null
+  percent: number | null
 }
 
 export type PriceCreate = {
   productId: number
-  buy?: number
-  sell?: number
+  buy?: bigint
+  sell?: bigint
   percent?: number
 }
 
 export type PriceUpdate = {
   id: number
   productId: number
-  buy?: number
-  sell?: number
+  buy?: bigint
+  sell?: bigint
   percent?: number
 }
