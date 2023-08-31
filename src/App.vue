926b2e320372da4c9bebb01343c8e79fac45455f
Majid Mohamadi <m.shutdown57@gmail.com>
2023-08-31 16:58:56 +0330

    Cleanup

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,5 @@
   <router-view />
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'App'
-
-  // async mounted () {
-  //   // @ts-ignore
-  //   const { database } = window
-  //   await database.init()
-  // }
-})
+<script setup lang="ts">
 </script>
