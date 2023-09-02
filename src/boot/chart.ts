bd2c5b384d9d87138429f876741264853bd22a78
Majid Mohamadi <m.shutdown57@gmail.com>
2023-09-02 21:15:45 +0330

    Added vue chart

---
+++ src/boot/chart.ts
@@ -0,0 +1,10 @@
+import { boot } from 'quasar/wrappers'
+import VueChart from 'vue-chartjs'
+
+// "async" is optional;
+// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
+export default boot(async (/* { app } */) => {
+  // something to do
+})
+
+export { VueChart }
