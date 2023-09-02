7ddc461fcb899afa2bb8ff1b3487b7a1a9b0dd6d
Majid Mohamadi <m.shutdown57@gmail.com>
2023-09-02 21:16:05 +0330

    Added bar chart sample component

---
+++ src/components/chart/BarComponent.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <Bar
+      id="my-chart"
+      :options="chartOptions"
+      :data="chartData"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { BarOptions, BarData } from 'src/interface/chart'
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+const { chartData, chartOptions } = defineProps<{chartData: BarData, chartOptions: BarOptions}>()
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+</script>
