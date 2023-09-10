2d3eed595b34fc3a79e056c79d5135a8b1851bb0
Majid Mohamadi <m.shutdown57@gmail.com>
2023-09-10 22:41:26 +0330

    Added try catch blocks

--- src/store/invoice/actions.ts
+++ src/store/invoice/actions.ts
@@ -7,64 +7,91 @@ const { database } = window
 
 const actions: ActionTree<InvoiceStateInterface, StateInterface> = {
   async getSellInvoices (context, payload = {}): Promise<void> {
-    if (!('limit' in payload)) {
-      payload.limit = 20
+    try {
+      if (!('limit' in payload)) {
+        payload.limit = 20
+      }
+      if (!('offset' in payload)) {
+        payload.offset = 0
+      }
+      if (!('buy' in payload)) {
+        payload.buy = false
+      }
+      const data = await database.invoice('all', payload)
+      context.commit('INVOICES_SET', data)
+    } catch (err) {
+      console.error(err)
     }
-    if (!('offset' in payload)) {
-      payload.offset = 0
-    }
-    if (!('buy' in payload)) {
-      payload.buy = false
-    }
-    const data = await database.invoice('all', payload)
-    context.commit('INVOICES_SET', data)
   },
 
   async getBuyInvoices (context, payload = {}): Promise<void> {
-    if (!('limit' in payload)) {
-      payload.limit = 20
+    try {
+      if (!('limit' in payload)) {
+        payload.limit = 20
+      }
+      if (!('offset' in payload)) {
+        payload.offset = 0
+      }
+      if (!('buy' in payload)) {
+        payload.buy = true
+      }
+      const data = await database.invoice('all', payload)
+      context.commit('INVOICES_SET', data)
+    } catch (err) {
+      console.error(err)
     }
-    if (!('offset' in payload)) {
-      payload.offset = 0
-    }
-    if (!('buy' in payload)) {
-      payload.buy = true
-    }
-    const data = await database.invoice('all', payload)
-    context.commit('INVOICES_SET', data)
   },
 
   async createInvoice (context, payload): Promise<void> {
-    const data = await database.invoice('create', payload)
-    context.commit('INVOICE_SET', data)
+    try {
+      const data = await database.invoice('create', payload)
+      context.commit('INVOICE_SET', data)
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   async getInvoice (context, payload): Promise<void> {
-    const data = await database.invoice('byId', payload)
-    context.commit('INVOICE_SET', data)
+    try {
+      const data = await database.invoice('byId', payload)
+      context.commit('INVOICE_SET', data)
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   async updateInvoice ({ dispatch }, payload): Promise<void> {
-    console.log(payload)
-    await database.invoice('update', payload)
-    const { id } = payload
-    await dispatch('getInvoice', { id: parseInt(id) })
+    try {
+      await database.invoice('update', payload)
+      const { id } = payload
+      await dispatch('getInvoice', { id: parseInt(id) })
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   async upsertProducts (_, payload): Promise<void> {
-    const { id } = payload
-    await database.invoice('upsertProducts', {
-      id,
-      products: payload.products,
-      prepayment: JSON.parse(JSON.stringify(payload.prepayment)),
-      installmentQuantity: JSON.parse(JSON.stringify(payload.installmentQuantity)),
-      installment: JSON.parse(JSON.stringify(payload.installment))
-    })
+    try {
+      const { id } = payload
+      await database.invoice('upsertProducts', {
+        id,
+        products: payload.products,
+        prepayment: JSON.parse(JSON.stringify(payload.prepayment)),
+        installmentQuantity: JSON.parse(JSON.stringify(payload.installmentQuantity)),
+        installment: JSON.parse(JSON.stringify(payload.installment))
+      })
+    } catch (err) {
+      console.error(err)
+    }
   },
 
   async count (context, payload): Promise<void> {
-    const data = await database.invoice('count', payload)
-    context.commit('INVOICE_COUNT', data)
+    try {
+      const data = await database.invoice('count', payload)
+      context.commit('INVOICE_COUNT', data)
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
 
