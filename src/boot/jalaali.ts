a78ce04235095503ae0175de479cb6521b797a14
Majid Mohamadi <m.shutdown57@gmail.com>
2023-09-16 19:19:51 +0330

    Cleanup

--- src/boot/jalaali.ts
+++ src/boot/jalaali.ts
@@ -1,14 +1,15 @@
 import { boot } from 'quasar/wrappers'
 import * as moment from 'moment-jalaali'
 
-const datetime = (value: string|undefined): string => moment(value).format('jYYYY/jM/jD HH:mm')
+const datetime = (value: string | undefined): string =>
+  moment(value).format('jYYYY/jM/jD HH:mm')
 
-const date = (value: string|undefined): string => moment(value).format('jYYYY/jM/jD')
+const date = (value: string | undefined): string =>
+  moment(value).format('jYYYY/jM/jD')
 
-const time = (value: string|undefined): string => moment(value).format('HH:mm')
+const time = (value: string | undefined): string =>
+  moment(value).format('HH:mm')
 
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
 export default boot(async () => {
   // something to do
 })
--- src/boot/persianTools.ts
+++ src/boot/persianTools.ts
@@ -1,17 +1,10 @@
 import { boot } from 'quasar/wrappers'
+import * as persianTools from '@persian-tools/persian-tools'
 import { digitsEnToFa, addCommas } from '@persian-tools/persian-tools'
 
-// declare module '@vue/runtime-core' {
-//   interface ComponentCustomProperties {
-//     $persianTools: object;
-//   }
-// }
-
-// "async" is optional;
-// more info on params: https://v2.quasar.dev/quasar-cli/boot-files
-export default boot(async () => {
-//   app.config.globalProperties.$persianTools = persianTools
-//   app.provide('persiantools.enToFa', digitsEnToFa as DigitsConverter)
+export default boot(async ({ app }) => {
+  app.config.globalProperties.$persianTools = persianTools
+  app.provide('persiantools', persianTools)
 })
 
 export {
